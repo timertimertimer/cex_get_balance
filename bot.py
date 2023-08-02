f1d975997fbdf9ba5fafb0125ae70ea8126316ab
timertimertimer <tginiyatullin2015@litsey2.ru>
2023-08-02 15:53:23 +0200

    Add reply button

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+from telebot import types
 from config import envs
 from bsc import MyBinance
 from kcn import MyKucoin
@@ -9,6 +10,8 @@ from okex import MyOkex
 bot = telebot.TeleBot(
     os.getenv('CEX_GET_BALANCE_BOT_TOKEN') or envs['CEX_GET_BALANCE_BOT_TOKEN'], parse_mode=None)
 
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup.add(types.KeyboardButton("/get_balance"))
 
 @bot.message_handler(commands=['get_balance'])
 def get_balance(message):
@@ -29,13 +32,13 @@ def get_balance(message):
         for ticker, value in cex_assets.items():
             if ticker != 'total':
                 amount = value['amount']
-                total_usd = value['total']
+                total_usd = value['total']        
                 s += f'{ticker} {amount} = {total_usd:.2f}$\n'
             else:
                 s += f'Total balance - {value:.2f}$'
                 balance += value
         bot.send_message(message.chat.id, s)
-    bot.send_message(message.chat.id, f'Total balance from CEXs - {balance:.2f}$')
+    bot.send_message(message.chat.id, f'Total balance from CEXs - {balance:.2f}$', reply_markup=markup)
 
 
 if __name__ == '__main__':
